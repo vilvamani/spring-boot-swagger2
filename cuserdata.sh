870a8af5960d7e81669f93ebc4437d91adedefad
vilvamani <41226243+vilvamani@users.noreply.github.com>
2021-02-17 16:49:40 +0530

    Update cuserdata.sh

--- cuserdata.sh
+++ cuserdata.sh
@@ -60,6 +60,10 @@ EOF
 
 yum install azure-cli -y
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 #Sign in with a managed identity
 az login --identity
 

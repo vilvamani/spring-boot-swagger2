d38b1fa330c7c969150837a5017ae085397c3114
vilvamani <41226243+vilvamani@users.noreply.github.com>
2021-04-27 11:12:14 +0530

    Update cuserdata.sh

--- cuserdata.sh
+++ cuserdata.sh
@@ -2,7 +2,7 @@
 pythonVersion=$(python --version)
 
 git clone https://github.com/vilvamani/quickstart-eks-boomi-molecule.git boomi_quickstart
-location=$(ls)
+location=$(ls -a)
 pip install -t . -r ./boomi_quickstart/functions/source/BoomiLicenseValidation/requirements.txt
 
 # Create output for S3 endpoint IP 

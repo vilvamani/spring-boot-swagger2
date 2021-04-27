cd1cc43ae3ac59f7719e44e991f5389212423167
vilvamani <41226243+vilvamani@users.noreply.github.com>
2021-04-27 12:20:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,8 @@ FAILED = "FAILED"
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
 if __name__ == '__main__':
-    print("The script has the account %s" % (sys.argv[0]))
-    print("The script has the username %s" % (sys.argv[1]))
-    print("The script has the password %s" % (sys.argv[2]))
-    sys.exit(0)
+	if sys.argv[1] == "token":
+		print("token")
+	else:
+		print("password")
+	sys.exit(0)

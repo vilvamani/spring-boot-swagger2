d581331517b2e6ba235204248133e3ad395a3e19
vilvamani <41226243+vilvamani@users.noreply.github.com>
2021-04-27 12:04:12 +0530

    Update cuserdata.sh

--- cuserdata.sh
+++ cuserdata.sh
@@ -3,7 +3,7 @@ pythonVersion=$(python --version)
 
 git clone https://github.com/vilvamani/quickstart-eks-boomi-molecule.git boomi_quickstart
 location=$(ls)
-pip install -t . -r ./boomi_quickstart/functions/source/BoomiLicenseValidation/requirements.txt
+#pip install -t . -r ./boomi_quickstart/functions/source/BoomiLicenseValidation/requirements.txt
 
 wget https://raw.githubusercontent.com/vilvamani/spring-boot-swagger2/master/test.py
 result=`python test.py "$MOLECULEUSERNAME" "$MOLECULEPASSWORD" "$MOLECULEACCOUNTID"`

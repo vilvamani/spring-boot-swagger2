80f48605f94fd4676e2df8e9ead0e738ec7c0947
vilvamani <41226243+vilvamani@users.noreply.github.com>
2021-04-27 10:53:27 +0530

    Update cuserdata.sh

--- cuserdata.sh
+++ cuserdata.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-serviceIp=$(python --version)
+pythonVersion=$(python --version)
+location=$(pwd)
+
+git clone https://github.com/vilvamani/quickstart-eks-boomi-molecule.git boomi_quickstart && cd boomi_quickstart/functions/source/BoomiLicenseValidation
+
+pip install -t . -r ./requirements.txt
+
 # Create output for S3 endpoint IP 
-echo \{\"loadBalancerIP\":\"$serviceIp\"\} > $AZ_SCRIPTS_OUTPUT_PATH
+echo \{\"pythonVersion\":\"$pythonVersion\"\, \"pwd\":\"$location\"\} > $AZ_SCRIPTS_OUTPUT_PATH

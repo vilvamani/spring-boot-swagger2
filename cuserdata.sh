3a7b316c100ee63e5dc54a4b664a702ba81bce34
vilvamani <41226243+vilvamani@users.noreply.github.com>
2021-04-28 18:22:59 +0530

    Update cuserdata.sh

--- cuserdata.sh
+++ cuserdata.sh
@@ -2,6 +2,6 @@
 result=$(az feature show --name AKS-IngressApplicationGatewayAddon --namespace Microsoft.ContainerService)
 
 
-status=jq --version
+status=$(jq --version)
 
 echo \{\"license_validation\":\"$status\"\,\"result\":\"$result\"\} > $AZ_SCRIPTS_OUTPUT_PATH

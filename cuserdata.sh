8f3a16d1615ff029104890eaf99a0858302c46c2
vilvamani <41226243+vilvamani@users.noreply.github.com>
2021-04-28 18:07:06 +0530

    Update cuserdata.sh

--- cuserdata.sh
+++ cuserdata.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
+result=$(az feature show --name AKS-IngressApplicationGatewayAddon --namespace Microsoft.ContainerService)
 
-git clone https://github.com/vilvamani/quickstart-eks-boomi-molecule.git boomi_quickstart
-pip install -t . -r ./boomi_quickstart/functions/source/BoomiLicenseValidation/requirements.txt
 
-wget https://raw.githubusercontent.com/vilvamani/spring-boot-swagger2/master/test.py
+status=`echo $result | jq .properties.state`
 
-az extension add --name aks-preview
-
-if [ $BOOMIAUTHENTICATIONTYPE == "token" ]
-then
-    result=`python test.py "$MOLECULEACCOUNTID" "BOOMI_TOKEN.$MOLECULEUSERNAME" "$BOOMIMFAAPITOKEN" MOLECULE 60`
-else
-    result=`python test.py "$MOLECULEACCOUNTID" "$MOLECULEUSERNAME" "$MOLECULEPASSWORD" MOLECULE 60`
-fi
-
-status=`echo $result|cut -d, -f1|awk -F':' '{ print $2 }'`
-token=`echo $result|cut -d, -f2|awk -F':' '{ print $2 }'`
-
-echo \{\"license_validation\":\"$status\"\, \"installation_token\":\"$token\"\} > $AZ_SCRIPTS_OUTPUT_PATH
+echo \{\"license_validation\":\"$status\"\} > $AZ_SCRIPTS_OUTPUT_PATH

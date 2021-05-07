7efea21b723e6bf2d354d217cdf99b633937a795
vilvamani <41226243+vilvamani@users.noreply.github.com>
2021-05-07 15:58:03 +0530

    Update vilva.groovy

--- vilva.groovy
+++ vilva.groovy
@@ -1,25 +1,55 @@
-def label = "maven-${UUID.randomUUID().toString()}" // TODO use POD_LABEL when available
-def mvnOpts = '-DskipTests -ntp -Darguments="-DskipTests -ntp"'
+#!/usr/bin/env groovy
+import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
+
+def label = "slave-${UUID.randomUUID().toString()}"
+def IMAGE_VERSION
+
+
+def getGitCredentials() {
+  def co = checkout(scm)
+  sh 'git config --local credential.helper "!p() { echo username=\\$GIT_USERNAME; echo password=\\$GIT_PASSWORD; }; p"'
+  GIT_COMMIT = co.GIT_COMMIT
+}
+
 
 podTemplate(label: label, containers: [
-  containerTemplate(name: 'maven', image: 'maven:3.6.1-jdk-8', ttyEnabled: true, command: 'cat')
-  ], volumes: [
-  persistentVolumeClaim(mountPath: '/root/.m2/repository', claimName: 'maven-repo', readOnly: false)
-  ]) {
+    containerTemplate(name: 'jnlp', image: '549050352176.dkr.ecr.us-east-1.amazonaws.com/translated-reviews/jenkins-jnlp:4.7-1', args: '${computer.jnlpmac} ${computer.name}'),
+    containerTemplate(name: 'awscli', image: 'amazon/aws-cli:2.2.3', command: 'cat', ttyEnabled: true),
+    containerTemplate(name: 'sonarqube', image: 'sonarsource/sonar-scanner-cli:4.6', command: 'cat', ttyEnabled: true),
+    containerTemplate(name: 'nodejs', image: 'node:11-alpine', command: 'cat', ttyEnabled: true),
+    containerTemplate(name: 'kaniko', image: 'gcr.io/kaniko-project/executor:debug', command: '/busybox/cat', ttyEnabled: true),
+  ],
+  volumes: [
+    configMapVolume(configMapName: 'docker-config', mountPath: '/kaniko/.docker/'),
+  ],
+  envVars: [],
+  annotations: [
+    podAnnotation(key: "iam.amazonaws.com/role", value: "arn:aws:iam::549050352176:role/translated-reviews-deploy")
+  ],
+  runAsUser: '1000',
+  runAsGroup: '1000'
+) {
+  timeout(time: 2, unit: 'HOURS') {
+    try {
+      node(label) {
+        properties([
+            disableConcurrentBuilds(),
+            parameters([booleanParam(defaultValue: false, description: 'Whether or not to wait for canary deployment + check', name: 'skipCanary')])
+        ])
 
-  node(label) {
-    stage('Checkout') {
-      checkout scm
-    }
-    stage('Release prepare') {
-      container('maven') {
-          sh "mvn -B ${mvnOpts} release:clean release:prepare -DreleaseVersion=${releaseVersion} -DdevelopmentVersion=${developmentVersion}"
+        container('nodejs') {
+          stage('Install') {
+              getGitCredentials()
+              IMAGE_VERSION = "${GIT_COMMIT}-${BRANCH_NAME}-${BUILD_NUMBER}"
+          }
+        }
       }
-    }
-    stage('Release perform') {
-      container('maven') {
-          sh "mvn -B ${mvnOpts} release:perform"
-      }
+
+    } catch (FlowInterruptedException interruptEx) {
+      echo "Job was cancelled"
+      throw interruptEx
+    } catch (failure) {
+      throw failure
     }
   }
 }

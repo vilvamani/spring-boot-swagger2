100884212e2932d8f18f31309551d4d77f83c58d
vilvamani <41226243+vilvamani@users.noreply.github.com>
2021-06-28 10:18:59 +0530

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -81,7 +81,7 @@ chmod -R 777 ${MoleculeLocalPath}/
 cat >/tmp/molecule_set_cluster_properties.sh <<EOF
 #!/bin/bash
 LOCAL_IVP4=$(curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2019-06-01&format=text")
-echo com.boomi.container.cloudlet.initialHosts=${!LOCAL_IVP4}[7800] >> ${MoleculeSharedDir}/Molecule_${MoleculeClusterName}/conf/container.properties
+echo com.boomi.container.cloudlet.initialHosts=[7800] >> ${MoleculeSharedDir}/Molecule_${MoleculeClusterName}/conf/container.properties
 echo com.boomi.container.cloudlet.clusterConfig=UNICAST >> ${MoleculeSharedDir}/Molecule_${MoleculeClusterName}/conf/container.properties
 echo com.boomi.deployment.quickstart=True >> ${MoleculeSharedDir}/Molecule_${MoleculeClusterName}/conf/container.properties
 EOF
